8f4238b9ac7eb61a52cd39a62970613deaf9b2b0
BertV1 <b3rt-pr0t0n@proton.me>
2023-09-26 21:52:58 +0200

    citizenlabs
    aaragh

--- main.py
+++ main.py
@@ -11,21 +11,34 @@
 
 amnesty = open(r'amnesty_tech.txt','r')
 meta = open(r'meta.txt','r')
+citizen_lab2 = open(r'citizens_lab2.txt','r')
+citizen_lab3 = open(r'citizens_lab3.txt','r')
 
 
 def get_doms(a_file):
     iocs_list = []
     pot_iocs = a_file.readlines()
+    print(pot_iocs)
     for pot_ioc in pot_iocs:
-        ioc = pot_ioc.strip()
+        pot_ioc = pot_ioc.strip()
         pot_ioc = pot_ioc.replace('[','').replace(']','')
-        iocs_list.append(pot_ioc)
+        if ' ' not in pot_ioc and pot_ioc != '':
+            iocs_list.append(pot_ioc)
+        elif '.' in pot_ioc:
+            if pot_ioc.find('.') < pot_ioc.find(' '):
+                pot_ioc = pot_ioc.split('\t')[0]
+                iocs_list.append(pot_ioc)
     return iocs_list
 
 amnesty_ioc_list = get_doms(amnesty)
 meta_ioc_list = get_doms(meta)
+cl3_ioc_list = get_doms(citizen_lab3)
 
-
+x=0
+for ioc in cl3_ioc_list:
+    if x<10:
+        print(ioc)
+    x+=1
 
 # depending on which format the original list has
 # function to parse them
